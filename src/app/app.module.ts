43374071ea5360aea78aaab3effa33d09cfca1be
Gabriel Monteiro <gmonteeeiro@gmail.com>
2023-03-18 16:15:59 -0300

    Cria 'Página em construção'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 import { UnderConstructionComponent } from './shared/components/under-construction/under-construction.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { UnderConstructionComponent } from './shared/components/under-constructi
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
