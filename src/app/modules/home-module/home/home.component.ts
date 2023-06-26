57154c2cd9adb64bfb143927735b89d9ebdce0c1
Gabriel Monteiro <gabsdocompiuter@gmail.com>
2023-06-26 14:45:53 -0300

    Add dinamic social media links by constant

--- src/app/modules/home-module/home/home.component.ts
+++ src/app/modules/home-module/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SocialMedias } from 'src/app/shared/constants/social_media.constant';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  socialMedias: Array<any> = SocialMedias;
 
   constructor() { }
 
---
+++ src/app/shared/constants/social_media.constant.ts
@@ -0,0 +1,36 @@
+export const SocialMedias = [
+  {
+    name: 'Instagram',
+    url: 'https://instagram.com/oichegamos',
+    icon: 'assets/icons/instagram.png',
+  },
+  {
+    name: 'Pinterest',
+    url: '',
+    icon: 'assets/icons/pinterest.png',
+    disabled: true,
+  },
+  {
+    name: 'Spotify',
+    url: '',
+    icon: 'assets/icons/spotify.png',
+    disabled: true,
+  },
+  {
+    name: 'TikTok',
+    url: 'https://tiktok.com/oichegamos',
+    icon: 'assets/icons/tiktok.png',
+  },
+  {
+    name: 'Twitter',
+    url: 'https://twitter.com/oichegamos',
+    icon: 'assets/icons/twitter.png',
+    disabled: true,
+  },
+  {
+    name: 'YouTube',
+    url: 'https://youtube.com/@oichegamos',
+    icon: 'assets/icons/youtube.png',
+  },
+
+];
