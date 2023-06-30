cf7d89724f5d647105496033e6c81b71dabdd3f2
Gabriel Monteiro <gabsdocompiuter@gmail.com>
2023-06-30 17:50:38 -0300

    Create our-numbers component

--- src/app/modules/home-module/home-module.module.ts
+++ src/app/modules/home-module/home-module.module.ts
@@ -2,12 +2,14 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LastPostsComponent } from './components/last-posts/last-posts.component';
+import { OurNumbersComponent } from './components/our-numbers/our-numbers.component';
 
 
 
 @NgModule({
   declarations: [
     LastPostsComponent,
+    OurNumbersComponent,
   ],
   imports: [
     CommonModule,
@@ -15,6 +17,7 @@ import { LastPostsComponent } from './components/last-posts/last-posts.component
   ],
   exports: [
     LastPostsComponent,
+    OurNumbersComponent,
   ]
 })
 export class HomeModule { }
