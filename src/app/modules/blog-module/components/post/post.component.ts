8d9b1b08bdb38f3794ad746c5532dcde2a39953b
Gabriel Monteiro <gabsdocompiuter@gmail.com>
2023-07-16 11:57:46 -0300

    Read post from backend

--- src/app/modules/blog-module/components/post/post.component.ts
+++ src/app/modules/blog-module/components/post/post.component.ts
@@ -1,4 +1,7 @@
+import { Location } from '@angular/common';
+import { PostsService } from './../../../../shared/services/posts.service';
 import { Component, OnInit } from '@angular/core';
+import { IPost } from 'src/app/shared/interfaces/post.interface';
 
 @Component({
   selector: 'app-post',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private location: Location,
+    private postsService: PostsService,
+  ) { }
+
+  public post: IPost = {} as IPost;
+  public isLoading: boolean = true;
+  public postNotFound: boolean = false;
+
+  public get postSlug(): string {
+    const pathSplited = this.location.path().split('/');
+    return pathSplited[pathSplited.length - 1];
+  }
 
   ngOnInit(): void {
+    this.loadPost();
+  }
+
+  loadPost(): void {
+    this.postsService.getPostBySlug(this.postSlug)
+      .subscribe({
+        next: (post: IPost) => this.post = post,
+        error: err => this.postNotFound = true,
+        complete: () => this.isLoading = false
+      });
   }
 
 }
