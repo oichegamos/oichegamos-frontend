01956f2277474a5e15f99d66237f6d07d0bba3c8
Gabriel Monteiro <gabsdocompiuter@gmail.com>
2023-07-17 10:57:20 -0300

    Blur behind navbar if is post page

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { routes } from '../../constants/routes.constant';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
@@ -9,8 +10,15 @@ import { routes } from '../../constants/routes.constant';
 export class NavbarComponent implements OnInit {
 
   public appRoutes = routes;
+  // public isPostPage: boolean = false;
 
-  constructor() { }
+  get isPostPage(): boolean {
+    return this.location.path().includes(this.appRoutes.blog.home)
+  }
+
+  constructor(
+    private location: Location,
+  ) { }
 
   ngOnInit(): void {
   }
